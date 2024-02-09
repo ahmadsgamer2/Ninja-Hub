af79943fe600ef8171c27a6fd5ee0a1bab2cb387
ahmadsgamer2 <107987829+ahmadsgamer2@users.noreply.github.com>
2024-02-09 12:48:31 +0400

    Create Theme.lua

---
+++ NinjaLib/Theme.lua
@@ -0,0 +1,19 @@
+local Themes = {
+  ["Default"] = {
+    ["Color Background 1"] = Color3.fromRGB(10, 10, 10),
+    ["Color Background 2"] = Color3.fromRGB(30, 30, 30),
+    ["Color Theme"] = Color3.fromRGB(247, 22, 22),
+    ["Color Text"] = Color3.fromRGB(240, 240, 240),
+    ["Color Dark Text"] = Color3.fromRGB(200, 200, 200),
+    ["Color Stroke"] = Color3.fromRGB(247, 22, 22),
+    ["Corner Radius"] = UDim.new(0, 5),
+    ["Font"] = {
+      Enum.Font.GothamBlack,
+      Enum.Font.GothamBold,
+      Enum.Font.GothamMedium
+    },
+    ["Stroke Thickness"] = .1,
+    ["Tween Style"] = Enum.EasingStyle.Quad
+  },
+}
+return Themes
