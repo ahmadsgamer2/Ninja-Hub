b6c1a87f52d349c9b6a0015eedcffa3fe02902d3
ahmadsgamer2 <107987829+ahmadsgamer2@users.noreply.github.com>
2024-02-09 13:05:07 +0400

    Update Theme.lua

--- NinjaLib/Theme.lua
+++ NinjaLib/Theme.lua
@@ -1,11 +1,11 @@
 local Themes = {
   ["Default"] = {
     ["Color Background 1"] = Color3.fromRGB(10, 10, 10),
-    ["Color Background 2"] = Color3.fromRGB(30, 30, 30),
-    ["Color Theme"] = Color3.fromRGB(247, 22, 22),
+    ["Color Background 2"] = Color3.fromRGB(7, 7, 7),
+    ["Color Theme"] = Color3.fromRGB(44, 10, 168),
     ["Color Text"] = Color3.fromRGB(240, 240, 240),
     ["Color Dark Text"] = Color3.fromRGB(200, 200, 200),
-    ["Color Stroke"] = Color3.fromRGB(247, 22, 22),
+    ["Color Stroke"] = Color3.fromRGB(44, 10, 168),
     ["Corner Radius"] = UDim.new(0, 5),
     ["Font"] = {
       Enum.Font.GothamBlack,
